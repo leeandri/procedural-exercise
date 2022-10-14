c8d6547e89cce737f6062907b1a73f6bcde2f114
leeandri <rl.tsirih@gmail.com>
2022-10-14 17:24:17 +0300

    commit all

---
+++ views/transactions.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Transactions</title>
+        <style>
+            *{
+                font-family: "Roboto Light" ;
+            }
+            table {
+                width: 100%;
+                border-collapse: collapse;
+                text-align: center;
+            }
+
+            table tr th, table tr td {
+                padding: 5px;
+                border: 1px #eee solid;
+            }
+
+            tfoot tr th, tfoot tr td {
+                font-size: 20px;
+            }
+
+            tfoot tr th {
+                text-align: right;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Check #</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (! empty($transactions)): ?>
+                    <?php foreach ($transactions as $transaction): ?>
+                        <tr>
+                            <td><?= formatDate($transaction['date']) ?></td>
+                            <td><?= $transaction['checkNumber'] ?></td>
+                            <td><?= $transaction['description'] ?></td>
+                            <td>
+                                <?php if ($transaction['amount'] < 0): ?>
+                                    <span style="color: red">
+                                        <?= formartDollarAmount($transaction['amount']) ?>
+                                    </span>
+                                <?php elseif ($transaction['amount'] > 0): ?>
+                                    <span style="color: green">
+                                        <?= formartDollarAmount($transaction['amount']) ?>
+                                    </span>
+                                <?php else: ?>
+                                    <?= formartDollarAmount($transaction['amount']) ?>
+                                <?php endif ?>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                <?php endif ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="3">Total Income:</th>
+                    <td><?= formartDollarAmount($totals['totalIncome'] ?? 0) ?></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Total Expense:</th>
+                    <td><?=  formartDollarAmount($totals['totalExpense'] ?? 0) ?></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Net Total:</th>
+                    <td><?=  formartDollarAmount($totals['netTotal'] ?? 0) ?></td>
+                </tr>
+            </tfoot>
+        </table>
+    </body>
+</html>
